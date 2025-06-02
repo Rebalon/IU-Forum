7449c622e881d2439d6b35afba85b023d9f5b98e
Thien0112 <128010271+Rebalon@users.noreply.github.com>
2025-06-02 20:40:22 +0700

    update

--- my-react-app/src/frontend/components/Header/Footer.tsx
+++ my-react-app/src/frontend/components/Header/Footer.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import '../../styles/footer.css';
+import { useNavigate } from 'react-router-dom';
 
 interface FooterProps {
   headerFooterColor: string;
 }
 
 const Footer: React.FC<FooterProps> = ({ headerFooterColor }) => {
+
+  const navigate = useNavigate();
+  const handleMenuClick = (e: React.MouseEvent, path?: string) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (path) {
+      navigate(path);
+    }
+  };
   return (
     <footer className="footer" style={{ backgroundColor: headerFooterColor }}>
       <div className="footer-left">
         <span>© 2024 IU Forum</span>
       </div>
       <div className="footer-right">
-        <a href="#" className="footer-link">About</a>
-        <a href="#" className="footer-link">Privacy</a>
-        <a href="#" className="footer-link">Terms</a>
-        <a href="#" className="footer-link">Help</a>
+        <a href="#" onClick={e => handleMenuClick(e, '/about')}>About</a>
+        <a href="#" onClick={e => handleMenuClick(e, '/privacy')}>Privacy</a>
+        <a href="#" onClick={e => handleMenuClick(e, '/terms')}>Terms</a>
+        <a href="#" onClick={e => handleMenuClick(e, '/help')}>Help</a>
       </div>
     </footer>
   );
--- my-react-app/src/frontend/components/Header/Navbar.tsx
+++ my-react-app/src/frontend/components/Header/Navbar.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import '../../styles/navbar.css';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
+
+  const navigate = useNavigate();
+
+  const handleMenuClick = (e: React.MouseEvent, path?: string) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (path) {
+      navigate(path);
+    }
+  };
+
   return (
     <nav className="navbar">
-      <a href="#" className="navLink">
-        Recent activity
-      </a>
-      <a href="#" className="navLink">
-        Clubs
-      </a>
-      <a href="#" className="navLink">
-        Chatbot
-      </a>
+      <a href="#" onClick={e => handleMenuClick(e, '/recent')}>Recent activity</a>
+      <a href="#" onClick={e => handleMenuClick(e, '/clubs')}>Clubs</a>
+      <a href="#" onClick={e => handleMenuClick(e, '/chatbot')}>Chatbot</a>
     </nav>
   );
 };
